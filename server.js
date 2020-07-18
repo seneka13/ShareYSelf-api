e101572b51c80525645839db065d0e8a7a3e35e4
Sam M <62692646+seneka13@users.noreply.github.com>
2020-07-18 20:20:26 +0600

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8220
+const port = process.env.PORT || 8220
 const defaultData = require('./defaultData')
 const cors = require('cors')
 const low = require('lowdb')
